d5901dbcbaa767c091079137f0bf195640c57120
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2017-12-15 13:39:26 +0700

    fixed post styling
    fixed filepath to stylesheet for posts and added read more link to news page

--- _site/js/main.js
+++ _site/js/main.js
@@ -10,11 +10,5 @@ $(document).ready(function(){
       //show mobile menu
       $('#mobile-menu').toggleClass('mobile-menu-visible')
   });
-  //show-hide button
-  $('.toggle-show-hide').click(function(){
-
-      //show mobile menu
-      $('#previous-updates').toggleClass('toggle-show-hide')
-  });
 
 });
